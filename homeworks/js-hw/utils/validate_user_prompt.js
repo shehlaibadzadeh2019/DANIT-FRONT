e1f6ef335eea4c45a3e818670d17ddab7ebb425b
Andrii <impex.ua@gmail.com>
2018-08-15 17:25:37 +0300

    JS-HW2

--- homeworks/js-hw/utils/validate_user_prompt.js
+++ homeworks/js-hw/utils/validate_user_prompt.js
@@ -1,16 +1,18 @@
 function promptSingleUsrInput(invitation){
     let usrInput = prompt(invitation);
     if (usrInput != null){
-       if (usrInput = getNumberOrFalse(usrInput)){
+       if (usrInput = getNaturalNumberOrFalse(usrInput)){
         return usrInput;
         }else {
             console.error("Wrong number format! Try again!");
             usrInput = promptSingleUsrInput(invitation);  
         } 
     }
-    console.error("User canceled!");
-    return false;
+    // console.error("User canceled!");
+    // return false;
+    throw new Error("User canceled!");
 }
+
 
 function getNaturalNumberOrFalse(usrInput){  
     if ((usrInput = Number(usrInput)) && Number.isInteger(usrInput) && usrInput >= 1){ 
@@ -23,14 +25,10 @@ function isMinMaxOrder(min, max){
     return min < max;
 }
 
-function isValidPair(min, max){
-    return getNaturalNumberOrFalse (min) && getNaturalNumberOrFalse(max) && isMinMaxOrder(min, max);
-}
-
 function promptNUsrInputs(n) {
     let res = [];
-    for (let i = 0; i < n; i++){
-        res.push(promptSingleUsrInput("Please input operand " + i + "of " + n + " operands:" ));
+    for (let i = 0; i < n; i++){ 
+        res.push(promptSingleUsrInput("Please input operand " + (i + 1) + " of " + n + " operands:" ));
     }
     return res;
 }
