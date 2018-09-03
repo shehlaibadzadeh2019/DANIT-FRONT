c4cf70f067208a9f11c44242f08ee41463dc6602
javasia <impex.ua@gmail.com>
2018-09-03 10:35:31 +0300

    folders reordered

--- js-hw/js_hw_3/js/factor.js
+++
@@ -1,6 +0,0 @@
-function getFactorial (n){
-    if (n == 0) return 1;
-    return n * getFactorial (--n);
-}
-
-console.log(getFactorial(+prompt("Please enter a number to calculate its factorial:")));
