7493b560ae115deced55b1a9293e84c34bb2d31d
javasia <impex.ua@gmail.com>
2018-12-28 21:54:58 +0200

    stringsRearrengement (in progress)

--- homeworks/js-hw/algorythms/stringsRearrengement/stringsRearrangement.js
+++ homeworks/js-hw/algorythms/stringsRearrengement/stringsRearrangement.js
@@ -6,7 +6,7 @@ function stringsRearrangement(inputArray) {
 	// 	i++;
 	// }
 	// return res;
-	return (next(0, 0, inputArray, 0, inputArray.length));
+	return (next(0, 0, inputArray, 0, inputArray.length - 1));
 }
 
 function next (currentCharNum, currentElementIdx, elements, currentCount, maxCount){
@@ -17,7 +17,11 @@ function next (currentCharNum, currentElementIdx, elements, currentCount, maxCou
 	mask[currentCharNum] = `[^${currentElement[currentCharNum]}]`;
 	mask = new RegExp(mask.join(''));
 	//elements = elements.slice(0, currentElementIdx).concat(elements.slice(currentElementIdx + 1));
-	if (next(0, 0, elements.filter(el => mask.test(el)), currentCount+1, maxCount)) return true;
+	//if (next(0, 0, elements.filter(el => mask.test(el)), currentCount+1, maxCount)) return true;
+	if (elements.some(el => mask.test(el))){
+		const newElements = elements.slice(0, currentElementIdx).concat(elements.slice(currentElementIdx + 1));
+		return next (0, newElements.findIndex(el => mask.test(el)), newElements, currentCount + 1, maxCount);
+	}
 	if (currentCharNum < currentElement.length - 1) return next (++currentCharNum, currentElementIdx, elements, currentCount, maxCount);
 	if (currentElementIdx < elements.length - 1) return next (0, ++currentElementIdx, elements, currentCount, maxCount);
 	return false;
