7071ead0f5561c9596e53b63d80e5d0a17f78c67
javasia <impex.ua@gmail.com>
2019-01-04 23:10:07 +0200

    optimized

--- homeworks/js-hw/algorythms/stringsRearrengement/stringsRearrangement.js
+++ homeworks/js-hw/algorythms/stringsRearrengement/stringsRearrangement.js
@@ -1,27 +1,22 @@
 function stringsRearrangement(inputArray) {
-	return rearrange("", 0, 0, inputArray, 0, inputArray.length);
+	return rearrange("", 0, 0, inputArray);
 }
 
-function rearrange(previuosWord, charIdx, wordIdx, words, count, wordsLength) {
-	if (count === wordsLength) return true;
-	if (words.length === 0) return false;
+function rearrange(previuosWord, charIdx, wordIdx, words) {
+	if (words.length === 0) return true;
 	const mask = getMask(previuosWord, charIdx);
 	const nextElementIdx = words.findIndex((el, idx) => idx >= wordIdx && mask.test(el));
 	if (~nextElementIdx) {
 		let tempWords = Array.from(words);
-		if (rearrange(tempWords.splice(nextElementIdx, 1)[0], 0, 0, tempWords, count + 1, wordsLength)) return true;
-		if (wordIdx < words.length - 1) {
-			return rearrange(previuosWord, 0, wordIdx + 1, words, count, wordsLength);
-		}
+		if (rearrange(tempWords.splice(nextElementIdx, 1)[0], 0, 0, tempWords)) return true;
+		if (wordIdx < words.length - 1) return rearrange(previuosWord, 0, wordIdx + 1, words);
 	}
-	if (charIdx < previuosWord.length - 1) {
-		return rearrange(previuosWord, charIdx + 1, wordIdx, words, count, wordsLength);
-	}
+	if (charIdx < previuosWord.length - 1) return rearrange(previuosWord, charIdx + 1, wordIdx, words);
 	return false;
 }
 
-function getMask(word, charNumber) {
-	return word ? new RegExp(word.slice(0, charNumber)
-		.concat(`[^${word.charAt(charNumber)}]`)
-		.concat(word.slice(charNumber + 1))) : new RegExp(".*");
+function getMask(word, charIdx) {
+	return word ? new RegExp(word.slice(0, charIdx)
+		.concat(`[^${word.charAt(charIdx)}]`)
+		.concat(word.slice(charIdx + 1))) : new RegExp(".*");
 }
