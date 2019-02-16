c96b911a1429f5442655c8bb1fc0a8d3d9fdca64
javasia <impex.ua@gmail.com>
2019-02-16 18:13:39 +0200

    test of StringRearrangement - excessive function abolished

--- codesignal/stringsRearrengement/stringsRearrangement.js
+++ codesignal/stringsRearrengement/stringsRearrangement.js
@@ -1,16 +1,13 @@
-let stringsRearrangement = (inputArray) => rearrange("", 0, 0, inputArray);
-
-function rearrange(previuosWord, charIdx, wordIdx, words) {
+let stringsRearrangement = (words, previuosWord="", charIdx=0, wordIdx=0) => {
 	if (words.length === 0) return true;
-	const mask = previuosWord ? new RegExp(previuosWord.slice(0, charIdx)
+	    const mask = previuosWord ? new RegExp(previuosWord.slice(0, charIdx)
 			.concat(`[^${previuosWord.charAt(charIdx)}]`)
 			.concat(previuosWord.slice(charIdx + 1))) : new RegExp(".*");
-	const nextWordIdx = words.findIndex((el, idx) => idx >= wordIdx && mask.test(el));
+	    const nextWordIdx = words.findIndex((el, idx) => idx >= wordIdx && mask.test(el));
 	if (~nextWordIdx) {
-		let tempWords = Array.from(words);
-		if (rearrange(tempWords.splice(nextWordIdx, 1)[0], 0, 0, tempWords)) return true;
-		if (wordIdx < words.length - 1) return rearrange(previuosWord, 0, wordIdx + 1, words);
+            if (stringsRearrangement (words.filter((w, i) => i != nextWordIdx), words[nextWordIdx], 0, 0)) return true;
+		    if (wordIdx < words.length - 1) return stringsRearrangement(words, previuosWord, 0, wordIdx + 1);
 	}
-	if (charIdx < previuosWord.length - 1) return rearrange(previuosWord, charIdx + 1, wordIdx, words);
+	if (charIdx < previuosWord.length - 1) return stringsRearrangement(words, previuosWord, charIdx + 1, wordIdx);
 	return false;
 }
--- codesignal/stringsRearrengement/stringsRearrangement.test.js
+++ codesignal/stringsRearrengement/stringsRearrangement.test.js
@@ -1,37 +1,49 @@
 let i = 1;
 
 describe(`stringsRearrangement`,  () => {
+
   it(`test${i++} input: ["aba","bbb","bab"] => false `,  () =>
     chai.assert.isFalse(stringsRearrangement(["aba","bbb","bab"]))
   );
+
   it(`test${i++} input: ["ab", "bb", "aa"] => true `,  () =>
     chai.assert.isTrue(stringsRearrangement(["ab", "bb", "aa"]))
   );
+
   it(`test${i++} input: ["q", "q"] => false `,  () =>
     chai.assert.isFalse(stringsRearrangement(["q", "q"]))
   );
+
   it(`test${i++} input:["zzzzab", "zzzzbb", "zzzzaa"] => true `,  () =>
     chai.assert.isTrue(stringsRearrangement(["zzzzab", "zzzzbb", "zzzzaa"]))
   );
+
   it(`test${i++} input: ["ab", "ad", "ef", "eg"] => false `,  () =>
     chai.assert.isFalse(stringsRearrangement(["ab", "ad", "ef", "eg"]))
   );
+
   it(`test${i++} input:["abc", "bef", "bcc", "bec", "bbc", "bdc"] => true `,  () =>
     chai.assert.isTrue(stringsRearrangement(["abc", "bef", "bcc", "bec", "bbc", "bdc"]))
   );
+
   it(`test${i++} input: ["abc", "abx", "axx", "abc"] => false `,  () =>
     chai.assert.isFalse(stringsRearrangement(["abc", "abx", "axx", "abc"]))
   );
+
   it(`test${i++} input:["abc", "abx", "axx", "abx", "abc"] => true `,  () =>
     chai.assert.isTrue(stringsRearrangement(["abc", "abx", "axx", "abx", "abc"]))
   );
+
   it(`test${i++} input:["f", "g", "a", "h"] => true `,  () =>
     chai.assert.isTrue(stringsRearrangement(["f", "g", "a", "h"]))
   );
+
   it(`test${i++} input:["ff", "gf", "af", "ar", "hf"] => true `,  () =>
     chai.assert.isTrue(stringsRearrangement(["ff", "gf", "af", "ar", "hf"]))
   );
+
   it(`test${i++} input:["a", "b", "c"] => true `,  () =>
     chai.assert.isTrue(stringsRearrangement(["a", "b", "c"]))
   );
+  
 });
