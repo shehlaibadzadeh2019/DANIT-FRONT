78ec3b22c79bc695c7d69216585d19b524813a28
javasia <impex.ua@gmail.com>
2019-02-16 15:00:25 +0200

    test of StringRearrangement: structure refactored

--- codesignal/stringsRearrengement/stringsRearrangement.test.js
+++ codesignal/stringsRearrengement/stringsRearrangement.test.js
@@ -1,67 +1,37 @@
 let i = 1;
 
 describe(`stringsRearrangement`, function () {
-  it(`test${i++} input: ["aba","bbb","bab"] false `, function () {
+  it(`test${i++} input: ["aba","bbb","bab"] => false `, function () {
     chai.assert.isFalse(stringsRearrangement(["aba","bbb","bab"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input: ["ab", "bb", "aa"] true `, function () {
+  it(`test${i++} input: ["ab", "bb", "aa"] => true `, function () {
     chai.assert.isTrue(stringsRearrangement(["ab", "bb", "aa"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input: ["q", "q"] false `, function () {
+  it(`test${i++} input: ["q", "q"] => false `, function () {
     chai.assert.isFalse(stringsRearrangement(["q", "q"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input:["zzzzab", "zzzzbb", "zzzzaa"] true `, function () {
+  it(`test${i++} input:["zzzzab", "zzzzbb", "zzzzaa"] => true `, function () {
     chai.assert.isTrue(stringsRearrangement(["zzzzab", "zzzzbb", "zzzzaa"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input: ["ab", "ad", "ef", "eg"] false `, function () {
+  it(`test${i++} input: ["ab", "ad", "ef", "eg"] => false `, function () {
     chai.assert.isFalse(stringsRearrangement(["ab", "ad", "ef", "eg"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input:["abc", "bef", "bcc", "bec", "bbc", "bdc"] true `, function () {
+  it(`test${i++} input:["abc", "bef", "bcc", "bec", "bbc", "bdc"] => true `, function () {
     chai.assert.isTrue(stringsRearrangement(["abc", "bef", "bcc", "bec", "bbc", "bdc"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input: ["abc", "abx", "axx", "abc"] false `, function () {
+  it(`test${i++} input: ["abc", "abx", "axx", "abc"] => false `, function () {
     chai.assert.isFalse(stringsRearrangement(["abc", "abx", "axx", "abc"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input:["abc", "abx", "axx", "abx", "abc"] true `, function () {
+  it(`test${i++} input:["abc", "abx", "axx", "abx", "abc"] => true `, function () {
     chai.assert.isTrue(stringsRearrangement(["abc", "abx", "axx", "abx", "abc"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input:["f", "g", "a", "h"] true `, function () {
+  it(`test${i++} input:["f", "g", "a", "h"] => true `, function () {
     chai.assert.isTrue(stringsRearrangement(["f", "g", "a", "h"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input:["ff", "gf", "af", "ar", "hf"] true `, function () {
+  it(`test${i++} input:["ff", "gf", "af", "ar", "hf"] => true `, function () {
     chai.assert.isTrue(stringsRearrangement(["ff", "gf", "af", "ar", "hf"]));
   });
-});
-
-describe(`stringsRearrangement`, function () {
-  it(`test${i++} input:["a", "b", "c"] true `, function () {
+  it(`test${i++} input:["a", "b", "c"] => true `, function () {
     chai.assert.isTrue(stringsRearrangement(["a", "b", "c"]));
   });
 });
